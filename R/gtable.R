32ef07e54c1c3ae30ee1d9f17e880b859527db19
John Verzani <jverzani@gmail.com>
2011-10-11 08:02:55 -0400

    integrate in popup menu

--- R/gtable.R
+++ R/gtable.R
@@ -26,17 +26,34 @@ NULL
 }
 
 
+## helper to make treeview columns based on type of data
 make_treeview_column <- function(x, col_no, editable=FALSE, model=NULL) UseMethod("make_treeview_column")
 make_treeview_column.default <- function(x, col_no, editable=FALSE, model=NULL) {
   ## Return a tree view column instance to render x, located in 0-based col in model
   cellrenderer <- gtkCellRendererText()
   view_col <- gtkTreeViewColumnNew()
+  view_col$setResizable(TRUE)
   view_col$PackStart(cellrenderer, TRUE)
   view_col$AddAttribute(cellrenderer, "text", col_no)
+
+  ## we override built in label
+  event_box <- gtkEventBox()
+  event_box$SetVisibleWindow(FALSE)
+  label <- gtkLabel()
+  event_box$addEvents('all-events-mask')
+  event_box$add(label)
+  event_box$setAboveChild(TRUE)         # gets events to box
+  
+  view_col$setWidget(event_box)
   view_col
 }
 
-## XXX
+##' Class for gtable widget
+##'
+##' This class implements a few additional reference methods:
+##' \code{hide_names} to hide the header names;
+##' \code{remove_popup_menu} to remove the popup menu;
+##' 
 GTable <- setRefClass("GTable",
                       contains="GWidget",
                       fields=list(
@@ -69,7 +86,7 @@ GTable <- setRefClass("GTable",
                                 if(missing(items) || ncol(items) > 1)
                                   widget$SetRulesHint(TRUE)
                                 widget$SetEnableSearch(TRUE)
-
+                                widget$setSearchColumn(1 + is.null(icon.col) - 1L)
 
                                 ## we want column index, not name
                                 if(is.character(icon.col))
@@ -82,13 +99,13 @@ GTable <- setRefClass("GTable",
                                 if(is.numeric(tooltip.col))
                                   tooltip.col <- as.integer(tooltip.col)
 
-                                
                                 initFields(chosen_col=as.integer(chosen.col),
                                            icon_col = icon.col,
                                            tooltip_col=tooltip.col,
                                            change_signal="row-activated",
                                            default_expand=TRUE,
-                                           default_fill=TRUE
+                                           default_fill=TRUE,
+                                           toolkit=toolkit # needed here for gmenu call later
                                            )
 
 
@@ -121,6 +138,7 @@ GTable <- setRefClass("GTable",
                           sapply(get_valid_columns(), function(col) {
                             treeview_col <- make_treeview_column(DF[,col], col - 1L)
                             widget$insertColumn(treeview_col, pos = -1) # at end
+                            add_popup_menu_to_column(treeview_col)
                           })
                         },
                         make_icon_column=function() {
@@ -129,22 +147,82 @@ GTable <- setRefClass("GTable",
                           view.col <- gtkTreeViewColumnNew()
                           view.col$PackStart(cellrenderer, TRUE)
                           view.col$AddAttribute(cellrenderer, "stock-id", icon_col - 1L)
+                          event_box <- gtkEventBox() # need this for consistency
+                          label <- gtkLabel()
+                          event_box$add(label)
+                          view.col$setWidget(event_box)
                           view.col
                         },
+                        add_popup_menu_to_column=function(view.col) {
+                          "Add a popup menu to the column for sorting, ..."
+                          
+                          view.col$setClickable(TRUE)
+                          find_col_no <- function(view.col) {
+                            ind <- which(sapply(widget$getColumns(), function(i) identical(i, view.col)))
+                            ind - !is.null(icon_col)
+                          }
+                          actions <- list(sort_increasing=
+                                          gaction("Sort (increasing)", handler=function(h, ...) {
+                                            col_index <- find_col_no(view.col)
+                                            DF <- get_model()
+                                            ind <- order(DF[,col_index], decreasing=FALSE)
+                                            DF$setFrame(DF[][ind,])
+                                          }),
+                                          sort_decreasing=
+                                          gaction("Sort (decreasing)", handler=function(h, ...) {
+                                            col_index <- find_col_no(view.col)
+                                            DF <- get_model()
+                                            ind <- order(DF[,col_index], decreasing=TRUE)
+                                            DF$setFrame(DF[][ind,])
+                                          }),
+                                          gseparator(),
+                                          gaction("Rename column", handler=function(h,...) {
+                                            col_index <- find_col_no(view.col)
+                                            cur_nms <- get_names()
+                                            out <- ginput("Rename column", text=cur_nms[col_index], parent=widget)
+                                            if(nchar(out)) {
+                                              out <- make.names(c(cur_nms, out))
+                                              view.col$getWidget()$getChild()$setLabel(tail(out, n=1))
+                                            }
+                                          })
+                                          )
+                          popup <- gmenu(actions, popup=TRUE, toolkit=toolkit)
+                          event_box <- view.col$getWidget()
+                          ## This is a *real* hack, as the following doesn't
+                          ## work when I use event_box. NOt sure why
+                          ## not, seems like the right combination of
+                          ## arguments is given
+                          btn <- event_box$getParent()$getParent()$getParent()
+                          id <- gSignalConnect(btn, "button-press-event", f=function(w, e, ...) {
+                            if(e$button == 1 && e$type == GdkEventType['button-press'] - 1L) {
+                              popup$widget$popup(button=e$button, activate.time=e$time)
+                            }
+                            FALSE
+                          })
+                          btn$setData("popup_id", id)
+                        },
+                        remove_popup_menu=function() {
+                          "remove popup menu from column headers"
+                          sapply(widget$getColumns(), function(view.col) {
+                            btn <- view.col$getWidget()$getParent()$getParent()$getParent()
+                            if(!is.null(id <- btn$getData("popup_id")))
+                              gSignalHandlerDisconnect(btn, id)
+                          })
+                        },
                         set_selection_mode=function(mode=c("none","single","browse", "multiple", "extended")) {
-                          "Set the selection mode"
+                          "Helper: Set the selection mode"
                           sel_model <- widget$getSelection()
                           sel_model$setMode(GtkSelectionMode[match.arg(mode)])
                         },
                         not_these=function() {
-                          "Helper. Remove these indices due to icon_col, ..."
+                          "Helper: Remove these indices due to icon_col, ..."
                           x <- unlist(list(icon_col, tooltip_col))
                           if(is.null(x))
                             x <- integer(0)
                           x
                         },
                         get_valid_columns=function() {
-                          "get column indices less those for icons, tooltips, visible"
+                          "Helper: get column indices less those for icons, tooltips, visible"
                           DF <- get_model()
                           if(!is(DF, "RGtkDataFrame"))
                             return(NULL)
@@ -175,7 +253,7 @@ GTable <- setRefClass("GTable",
                             invoke_change_handler()
                         },
                         get_model=function() {
-                          "get rGtkDataFrame model"
+                          "Helper: get rGtkDataFrame model, which is filtered"
                           m <- widget$getModel()
                           if(is(m, "GtkTreeModelFilter"))
                             m$getModel() # get past filter
@@ -187,75 +265,83 @@ GTable <- setRefClass("GTable",
                         get_value=function(drop=TRUE, ...) {
                           "Get selected values by value (or character(0))"
                           vals <- get_items()[get_selected(), ]
-                        if(getWithDefault(drop, TRUE))
-                          vals[, chosen_col, drop=TRUE]
-                        else
-                          vals
-                      },
-                      set_value=function(value, ...) {
-                        "Set selected values by vector matching chosen.col"
-                        ind <- match(value, get_value(drop=TRUE))
-                        if(length(ind) == 1 && is.na(ind))
-                          return() ## no match
-                        set_index(ind)
-                      },
-                      get_index = function(...) {
-                        "Get index of selected rows or integer(0)"
-                        get_selected()
-                      },
-                      set_index = function(value,...) {
-                        "set selected values in value"
-                        set_selected(as.integer(value) - 1L)
-                      },
-                      get_items = function(i, j, ..., drop=TRUE) {
-                        DF <- get_model()[]
-                        ## we drop out some stuff
-                        DF[, get_valid_columns()][i,j]
-                      },
-                      set_items = function(value, i, j, ...) {
-                        if(missing(i) && missing(j)) {
-                          ## set a new data frame model
-                          ## we shove in ..visible for the last column q
-                          if(!is(value, "data.frame"))
-                            value <- as.data.frame(value)
-                          ## icons
-                          if(!is.null(icon_col)) 
-                            value[[icon_col]] <-  getStockIconByName(value[[icon_col]])
-                          ## visible column
-                          items <<- cbind(value, ..visible=rep(TRUE, nrow(value)))
-                          model <- rGtkDataFrame(items)
-                          filter <- model$filter()
-                          filter$setVisibleColumn(ncol(items) -1L) # last column
-                          clear_columns()
-                          widget$setModel(filter)
-                          make_columns()
-                          set_names(names(value)[get_valid_columns()])
-                        } else {
-                          df_model <- get_model()
-                          df_model[i,j] <- value ## hope case matches
-                        }
-                      },
-                      ## data store methods
-                      get_length=function() {
-                        get_dim()[2]
-                      },
-                      get_dim=function() {
-                        "Return dim of view (not data frame which may have extra information)"
-                        c(rows=dim(get_model())[1], columns=length(get_valid_columns()))
-                      },
-                      get_names=function() {
-                        sapply(widget$getColumns(), gtkTreeViewColumnGetTitle)
-                      },
-                      set_names =function(value) {
-                        ## check length
-                        m <- get_dim()[2]
-                        if(length(value) != m)
-                          return()
-                        ## adjust for icons
-                        if(!is.null(icon_col))
-                          value <- c("", value)
-                        mapply(gtkTreeViewColumnSetTitle, widget$getColumns(), value)
-                      },
+                          if(getWithDefault(drop, TRUE))
+                            vals[, chosen_col, drop=TRUE]
+                          else
+                            vals
+                        },
+                        set_value=function(value, ...) {
+                          "Set selected values by vector matching chosen.col"
+                          ind <- match(value, get_value(drop=TRUE))
+                          if(length(ind) == 1 && is.na(ind))
+                            return() ## no match
+                          set_index(ind)
+                        },
+                        get_index = function(...) {
+                          "Get index of selected rows or integer(0)"
+                          get_selected()
+                        },
+                        set_index = function(value,...) {
+                          "set selected values in value"
+                          set_selected(as.integer(value) - 1L)
+                        },
+                        get_items = function(i, j, ..., drop=TRUE) {
+                          DF <- get_model()[]
+                          ## we drop out some stuff
+                          DF[, get_valid_columns()][i,j]
+                        },
+                        set_items = function(value, i, j, ...) {
+                          if(missing(i) && missing(j)) {
+                            ## set a new data frame model
+                            ## we shove in ..visible for the last column q
+                            if(!is(value, "data.frame"))
+                              value <- as.data.frame(value)
+                            ## icons
+                            if(!is.null(icon_col)) 
+                              value[[icon_col]] <-  getStockIconByName(value[[icon_col]])
+                            ## visible column
+                            items <<- cbind(value, ..visible=rep(TRUE, nrow(value)))
+                            model <- rGtkDataFrame(items)
+                            filter <- model$filter()
+                            filter$setVisibleColumn(ncol(items) -1L) # last column
+                            clear_columns()
+                            widget$setModel(filter)
+                            make_columns()
+                            set_names(names(value)[get_valid_columns()])
+                          } else {
+                            df_model <- get_model()
+                            df_model[i,j] <- value ## hope case matches
+                          }
+                        },
+                        ## data store methods
+                        get_length=function() {
+                          get_dim()[2]
+                        },
+                        get_dim=function() {
+                          "Return dim of view (not data frame which may have extra information)"
+                          c(rows=dim(get_model())[1], columns=length(get_valid_columns()))
+                        },
+                        get_names=function() {
+                          sapply(widget$getColumns(), function(col) {
+                            label <- col$getWidget()$getChild()
+                            label$getLabel()
+                          })
+                        },
+                        set_names =function(value) {
+                          ## check length
+                          m <- get_dim()[2]
+                          if(length(value) != m)
+                            return()
+                          view_cols <- widget$getColumns()
+                          ## adjust for icons
+                          if(!is.null(icon_col))
+                            view_cols <- view_cols[-1]
+                          f <- function(col, nm) {
+                            label <- col$getWidget()$getChild()
+                            label$setLabel(nm)
+                          }
+                          mapply(f, view_cols, value)
+                        },
                         get_visible=function() {
                           ## return last column in DF
                           DF <- get_model()
@@ -298,7 +384,7 @@ GTable <- setRefClass("GTable",
                         },
                         add_handler_double_clicked=function(handler, action, ...) {
                           ## There is an oddity here. When using row-activated it does as desired unless
-                          ## we alos have addHandlerChanged called in which case this is always called. So
+                          ## we also have addHandlerChanged called in which case this is always called. So
                           ## we bypass and do button and mouse events as they arise
                           ## add_handler("row-activated", handler, action=action, ...)
 
