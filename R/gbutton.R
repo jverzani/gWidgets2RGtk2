15f7628599a8b0425c7ed8385d3a72871237dd6c
jverzani <jverzani@gmail.com>
2014-03-14 18:06:09 -0400

    put button in event box to see if colors work

--- R/gbutton.R
+++ R/gbutton.R
@@ -22,13 +22,15 @@ GButton <- setRefClass("GButton",
                               initialize=function(toolkit=NULL, text=NULL,  handler, action, container, ...) {
                                 
                                 widget <<- gtkButton()
+                                block <<- gtkEventBox()
+                                block$add(widget)
                                 toolkit <<- toolkit # otherwise next line fails to find toolkit for dispatch
                                 if(!is_empty(text))
                                   set_value(text)
 
 
                                 
-                                initFields(block=widget,
+                                initFields(
                                            change_signal="clicked"
                                            )
                                 
