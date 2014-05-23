47a90ac08f8de973afbaa436fbb8d3fcbbe9b4d8
jverzani <jverzani@gmail.com>
2014-05-23 07:36:34 -0400

    address stock image issues

--- R/gimage.R
+++ R/gimage.R
@@ -22,27 +22,19 @@ GImage <- setRefClass("GImage",
                       contains="GWidget",
                       fields=list(
                         "image_name"="ANY", # name (filename or stock)
-                        "stock_size"="integer"  # size if stock (GtkIconSize value)
-                        ),
+                          "stock_size"="integer"  # size if stock (GtkIconSize value)
+                          ),
                       methods=list(
-                        initialize=function(toolkit=NULL,
-                          filename = "", dirname = "", stock.id=NULL, size = "",
-                          handler=NULL, action=NULL, container=NULL, ...) {
-                          
-
-                          ## get file or stock
-                          stock_size <<- 0L
-                          if(!is.null(stock.id)) {
-                            size <- switch(toupper(size),
-                                           "MENU"= GtkIconSize["menu"],
-                                           "SMALL_TOOLBAR"= GtkIconSize["small-toolbar"],
-                                           "LARGE_TOOLBAR"= GtkIconSize["large-toolbar"],
-                                           "BUTTON"= GtkIconSize["button"],
-                                           "DND"= GtkIconSize["dnd"],
-                                           "DIALOG"= GtkIconSize["dialog"],
-                                           GtkIconSize["menu"])
-                            image_name <<- getStockIconByName(stock.id, toolkit=toolkit)
-                            stock_size <<- size
+                          initialize=function(toolkit=NULL,
+                              filename = "", dirname = "", stock.id=NULL, size = "",
+                              handler=NULL, action=NULL, container=NULL, ...) {
+                              
+                              
+                              ## get file or stock
+                              stock_size <<- GtkIconSize["large-toolbar"]
+                              if(!is.null(stock.id)) {
+                              set_stock_size(size)
+                            image_name <<- stock.id
                           } else {
                             ## piece together name
                             if(nchar(dirname))
@@ -50,8 +42,8 @@ GImage <- setRefClass("GImage",
                             if(file.exists(filename)) {
                               image_name <<- filename
                             } else {
-                              message(sprintf("File %s is not found", filename))
-                              stop()
+                                ## defaults to trying stock name
+                                image_name <<- filename
                             }
                           }
 
@@ -61,12 +53,13 @@ GImage <- setRefClass("GImage",
                           block$setVisibleWindow(FALSE)
                           block$add(widget)
 
-                          set_value(image_name)
 
                           add_to_parent(container, .self, ...)
 
                           initFields("change_signal"="button-press-event")
                           handler_id <<- add_handler_changed(handler, action)
+
+                              set_value(image_name)
 
                           callSuper(toolkit)
                         },
@@ -78,11 +71,24 @@ GImage <- setRefClass("GImage",
                             widget$setFromFile(value)
                           } else {
                             ## assume stock
-                            value <- getStockIconByName(value, toolkit=toolkit)
-                            widget$setFromStock(value, size=stock_size)
+                              value <- getStockIconByName(value)
+                              widget$setFromStock(value, size=stock_size)
+                              widget$show()
                           }
                           image_name <<- value
                         },
+                          set_stock_size=function(size) {
+                              ## set the stock image size
+                              size <- switch(toupper(size),
+                                             "MENU"= GtkIconSize["menu"],
+                                             "SMALL_TOOLBAR"= GtkIconSize["small-toolbar"],
+                                             "LARGE_TOOLBAR"= GtkIconSize["large-toolbar"],
+                                             "BUTTON"= GtkIconSize["button"],
+                                             "DND"= GtkIconSize["dnd"],
+                                             "DIALOG"= GtkIconSize["dialog"],
+                                             GtkIconSize["menu"])
+                              stock_size <<- size
+                          },
                         handler_widget=function() {
                           block
                         },
